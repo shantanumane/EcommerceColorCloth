2fe2502e677479e4463549d51249861a320b256c
Shantanu Personal <shantanumane01@gmail.com>
2024-01-04 23:13:40 +0530

    basic routes
    Signed-off-by: Shantanu Personal <shantanumane01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
-import Layout from './components/layout';
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -11,24 +10,16 @@ import Services from "./pages/services";
 
 function App() {
   return (
-    // <div>
-    //   <Layout></Layout>
-    // </div>
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="/About" component={<About />} />
+                <Route path="/About" element={<About />} />
                 <Route path="/Contact" element={<Contact />} />
                 <Route path="/Pricing" element={<Pricing />} />
                 <Route path="/Services" element={<Services />} />
-            </Route>
         </Routes>
     </BrowserRouter>
   );
 }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
 export default App;
 
