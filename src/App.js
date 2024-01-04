c687d1a6a1b645b9002dcc7214af16f3b003b130
Shantanu Personal <shantanumane01@gmail.com>
2024-01-04 23:07:17 +0530

    initial router setup
    Signed-off-by: Shantanu Personal <shantanumane01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import Layout from './components/layout';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Pricing from "./pages/pricing";
+import Services from "./pages/services";
+
 function App() {
   return (
-    <div className="App">
-      <Layout></Layout>
-    </div>
+    // <div>
+    //   <Layout></Layout>
+    // </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/About" component={<About />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/Pricing" element={<Pricing />} />
+                <Route path="/Services" element={<Services />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 export default App;
+
---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+const About = () => {
+    return <h1 style={{'fontWeight':'100px'}}>About</h1>;
+  };
+  
+  export default About;
