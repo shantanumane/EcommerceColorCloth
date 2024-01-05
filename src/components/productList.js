cbcd14c4fca5ee411fb981335c74e19cd6b8f8b6
Shantanu Personal <shantanumane01@gmail.com>
2024-01-05 15:11:14 +0530

    View All functionality added
    Signed-off-by: Shantanu Personal <shantanumane01@gmail.com>

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import Card from "./card";
+import { Link } from "react-router-dom";
 
 const ProductList=(props)=>{
     useEffect(()=>{
@@ -9,11 +10,16 @@ const ProductList=(props)=>{
         <div className="mx-10">
             <div class="grid grid-cols-1 md:grid-cols-4 gap-3">
                 {props.productList.map((product)=>(
-                    
-                    <div  className="relative grou">
-                        <Card imageUrl={product.imageUrl} title={product.productName} description={product.price} />
+                    <div  className="relative group">
+                        <Card product={product}  />
                     </div>
                 ))}
+            </div>
+            <div className="text-right">
+                {props.category!=='All Category'?<div>
+                    <Link to='/CategoryView'><button type="button" className="py-2.5 px-5 my-4 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">View More</button></Link>
+                </div>:''
+                }
             </div>
         </div>
     );
